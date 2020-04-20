a2ce936a1c72c5aa5befd3c8cfc5effec1afae95
Matthue <matthewjury2@gmail.com>
2020-04-20 16:03:37 -0400

    updated collision and some rotation

--- TetrisAttemptMonoGame/TetrisAttempt1.cs
+++ TetrisAttemptMonoGame/TetrisAttempt1.cs
@@ -8,181 +8,282 @@ using System.Threading;
 
 namespace TetrisAttemptMonoGame
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
-    public class Game1 : Game
-    {
-        static List<Tetromino> Tetrominos = new List<Tetromino>();
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-        Tetromino tetromino = new Tetromino();
-        static KeyboardState oldKeyboardState;
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
 
-        Texture2D background;
-        public Texture2D tile;
+		static KeyboardState oldKeyboardState;
+		static Random random = new Random();
+		static int row19;
 
-        public Game1()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-        }
+		static Tetromino.type CurrentPieceType;
+		static int CurrentRotation = 0;
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-            base.Window.Title = "Tetris by Matthew Jury";
-            graphics.PreferredBackBufferWidth = 790;
-            graphics.PreferredBackBufferHeight = 650;
-            this.IsMouseVisible = true;
-            graphics.ApplyChanges();
-            var callback = new TimerCallback(TimerTick);
-            var timer = new System.Threading.Timer(callback, null, 0,200);
-        }
-
-        private static void TimerTick(Object StateInfo)
-        {
-            for (int i = 1; i < Tetrominos.Count - 1; i++)
-            {
-                if (Tetrominos[i].Y < 22)
-                {
-                    Tetrominos[i].Y++;
-                }
+		static TimerCallback callback = new TimerCallback(TimerTick);
+		public static System.Threading.Timer timer = new System.Threading.Timer(callback, null, 0, 150);//TIMER!!!
 
 
-                if (i == Tetrominos.Count - 1) 
-                {
-                    if (Tetrominos[i - 1].Y + 1 == Tetrominos[i].Y)
-                    {
-                        NewPiece();
-                        break;
-                    }
-                    if (Tetrominos[i].Y == 22)
-                    {
-                        NewPiece();
-                    }
-                    
-                }
-            }
-        }
-        private static void updateInput()
-        {
-            KeyboardState NewKeyboardState = Keyboard.GetState();
-            for(int i = 1; i < Tetrominos.Count; i++)
-            {
-                if (i == Tetrominos.Count - 2)
-                {
-                    if (Tetrominos[i].Y == Tetrominos[i - 1].Y
-                        && Tetrominos[i].X == Tetrominos[i - 1].X)
-                    {
-                        Tetrominos.Add(new Tetromino());
-                        Tetrominos[i].Y -= 1;
-                        
-                        //break;
-                    }
-                    if (NewKeyboardState.IsKeyDown(Keys.Left)
-                    && oldKeyboardState.IsKeyUp(Keys.Left)
-                    && Tetrominos[i].X > 12)
-                    {
-                        Tetrominos[i].X--;
-                    }
-                    if (NewKeyboardState.IsKeyDown(Keys.Right)
-                    && oldKeyboardState.IsKeyUp(Keys.Right)
-                    && Tetrominos[i].X < 21)
-                    {
-                        Tetrominos[i].X++;
-                    }
-                    
-                }
-                //else
-                //{
-                //    NewPiece();
-                //}
-            }
-            
-            
-            oldKeyboardState = NewKeyboardState;
-
-        }
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-            tile = Content.Load<Texture2D>("tile");
-            background = Content.Load<Texture2D>("background");
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        /// 
-        
-        protected override void Update(GameTime gameTime)
-        {
-            MouseState MS = Mouse.GetState();
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-            if (MS.LeftButton == ButtonState.Pressed)
-                NewPiece();
-            updateInput();
-            
+		static bool collision = false;
 
 
-            // TODO: Add your update logic here
-            
-            base.Update(gameTime);
-        }
-        private static void NewPiece()
-        {
-            Tetrominos.Add(new Tetromino());
-        }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin();
-            if (Tetrominos.Count > 1) {
-                for (int i = 0; i < Tetrominos.Count; i++)
-                {
-                    spriteBatch.Draw(tile, new Vector2(Tetrominos[i].X * 25, Tetrominos[i].Y * 25), Color.Orange);
-                }
-            }
-            //spriteBatch.Draw(background, new Vector2(0,0), Color.White);
-            // TODO: Add your drawing code here
+		Texture2D background;
+		Texture2D tile;
 
 
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+		protected override void Initialize()
+		{
+			base.Initialize();
+			base.Window.Title = "Tetris by Matthew Jury";
+			graphics.PreferredBackBufferWidth = 250;
+			graphics.PreferredBackBufferHeight = 500;
+			this.IsMouseVisible = true;
+			graphics.ApplyChanges();
+			//Tetromino.PieceList.Add(new Tetromino(0, -10));
+ 
+			NewPiece();
+		}
 
+		private static void TimerTick(Object StateInfo)
+		{
+			CheckLine();
 
-            spriteBatch.End();
+			MouseState ms = Mouse.GetState();
+			if (ms.LeftButton == ButtonState.Pressed)
+			{
+				NewPiece();
+			}
+			int CurrentCount = (Tetromino.PieceList.Count);
+			collision = false;
+			int counter = 0;
+			for (int i = CurrentCount - 4; i < CurrentCount; i++)//check current piece
+			{
+				counter++;
+				if (counter <= 4 && i >= 0)//TIMER TICK MOVEMENT
+				{
+					Tetromino.PieceList[i].Y++;
+				}
+				for (int j = 0; j < Tetromino.PieceList.Count - 4; j++)//then scan for collision against every other piece
+				{
+					if (Tetromino.PieceList[i].Y == Tetromino.PieceList[j].Y - 1
+						&& Tetromino.PieceList[i].X == Tetromino.PieceList[j].X
+						|| Tetromino.PieceList[i].Y == 19)//COLLISION WITH ALL PIECES
+					{
+						collision = true;
+						break;
+					}
+				}
+			}
+		}
+		private static void CheckLine()
+		{
+			row19 = 0;
+			foreach (Tetromino piece in Tetromino.PieceList)
+			{
+				if (piece.Y == 19)
+				{
+					row19++;
+				}
+			}
 
-            base.Draw(gameTime);
-        }
-    }
+			if (row19 == 10)
+			{
+				LineClear(19);
+			}
+		}
+		private static void LineClear(int Row)
+		{
+			for (int i = 0; i < Tetromino.PieceList.Count; i++)
+			{
+				if (Tetromino.PieceList[i].Y == Row)
+				{
+					Tetromino.PieceList.RemoveAt(i);
+				}
+			}
+			foreach (Tetromino piece in Tetromino.PieceList)
+			{
+				piece.Y++;
+			}
+		}
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			background = Content.Load<Texture2D>("background");
+			tile = Content.Load<Texture2D>("tile");
+
+		}
+		protected override void UnloadContent()
+		{
+		}
+		protected override void Update(GameTime gameTime)
+		{
+			if (collision)
+				NewPiece();
+			collision = false;
+
+			KeyboardState OldKeyboardState;
+			KeyboardState NewKeyboardState = Keyboard.GetState();
+
+			//ROTATE//
+			if (NewKeyboardState.IsKeyUp(Keys.RightShift)
+				&& oldKeyboardState.IsKeyDown(Keys.RightShift))
+			{
+				RotatePiece(CurrentPieceType);
+			}
+			//////////
+			bool LeftCollision = false;
+			bool RightCollision = false;
+			for (int i = 0; i < Tetromino.PieceList.Count; i++)
+			{
+				if (i > Tetromino.PieceList.Count - 5)//IF CURRENT FALLING PIECE
+				{
+					if (Tetromino.PieceList[i].X == 0)
+					{
+						LeftCollision = true;
+					}
+					if (Tetromino.PieceList[i].X == 9)
+					{
+						RightCollision = true;
+					}
+					//===============CONTROLS===============
+					if (NewKeyboardState.IsKeyUp(Keys.Right)
+						&& oldKeyboardState.IsKeyDown(Keys.Right)
+						&& !RightCollision)//window bounds
+					{
+						 Tetromino.PieceList[i].X++;
+					}
+					if (NewKeyboardState.IsKeyUp(Keys.Left)
+						&& oldKeyboardState.IsKeyDown(Keys.Left)
+						&& !LeftCollision)//window bounds
+					{
+						Tetromino.PieceList[i].X--;
+					}
+					
+				}
+			}
+			base.Update(gameTime);
+			oldKeyboardState = NewKeyboardState;
+		}
+		private static void NewPiece()
+		{//================================TODO: ADD DIFFERENT TYPES OF SHAPES=====================
+			timer.Change(1000, 150);
+			//int typeInt = random.Next(0, 3);
+			int typeInt = 1;
+			CurrentRotation = 0;
+			if (typeInt == 0)//I
+			{
+				CurrentPieceType = Tetromino.type.I;
+				Tetromino.PieceList.Add(new Tetromino(4,0));
+				Tetromino.PieceList.Add(new Tetromino(4,1));
+				Tetromino.PieceList.Add(new Tetromino(4,2));
+				Tetromino.PieceList.Add(new Tetromino(4,3));
+			}
+			else if (typeInt == 1)//L
+			{
+				CurrentPieceType = Tetromino.type.L;
+				Tetromino.PieceList.Add(new Tetromino(4, 0));
+				Tetromino.PieceList.Add(new Tetromino(4, 1));
+				Tetromino.PieceList.Add(new Tetromino(4, 2));
+				Tetromino.PieceList.Add(new Tetromino(5, 2));
+			}
+			else if (typeInt == 2)//T
+			{
+				CurrentPieceType = Tetromino.type.T;
+				Tetromino.PieceList.Add(new Tetromino(5, 0));
+				Tetromino.PieceList.Add(new Tetromino(4, 1));
+				Tetromino.PieceList.Add(new Tetromino(5, 1));
+				Tetromino.PieceList.Add(new Tetromino(6, 1));
+			}
+
+			CheckLine();
+			ResetVals();
+		}
+		private static void RotatePiece(Tetromino.type CurrentType)
+		{
+			int currentPieceCount = Tetromino.PieceList.Count;
+
+			if (CurrentType == Tetromino.type.I)
+			{
+				if (CurrentRotation == 0)
+				{
+					Tetromino.PieceList[currentPieceCount - 1].X += 2;
+					Tetromino.PieceList[currentPieceCount - 1].Y -= 2;
+					Tetromino.PieceList[currentPieceCount - 2].X += 1;
+					Tetromino.PieceList[currentPieceCount - 2].Y -= 1;
+					//Tetromino.PieceList[currentPieceCount - 3].X--;
+					Tetromino.PieceList[currentPieceCount - 4].X -= 1;
+					Tetromino.PieceList[currentPieceCount - 4].Y++;
+					CurrentRotation = 90;
+					goto BreakOut;
+				}
+				if (CurrentRotation == 90)
+				{
+					Tetromino.PieceList[currentPieceCount - 1].X --;
+					Tetromino.PieceList[currentPieceCount - 2].X--;
+					Tetromino.PieceList[currentPieceCount - 2].Y--;
+					Tetromino.PieceList[currentPieceCount - 3].Y -= 2;
+					Tetromino.PieceList[currentPieceCount - 4].Y -= 3;
+					Tetromino.PieceList[currentPieceCount - 4].X++;
+
+					CurrentRotation = 0;
+					goto BreakOut;
+				}
+			}
+			if (CurrentType == Tetromino.type.L)
+			{
+				if (CurrentRotation == 0)
+				{
+					Tetromino.PieceList[currentPieceCount - 1].X -= 2;
+					Tetromino.PieceList[currentPieceCount - 1].Y--;
+					Tetromino.PieceList[currentPieceCount - 2].X --;
+					Tetromino.PieceList[currentPieceCount - 2].Y -= 2;
+					Tetromino.PieceList[currentPieceCount - 3].Y--;
+					Tetromino.PieceList[currentPieceCount - 3].X++;
+
+					CurrentRotation = 90;
+					goto BreakOut;
+				}
+				if (CurrentRotation == 90)
+				{
+
+					Tetromino.PieceList[currentPieceCount - 1].Y--;
+					Tetromino.PieceList[currentPieceCount - 2].X++;
+					Tetromino.PieceList[currentPieceCount - 3].Y++;
+					Tetromino.PieceList[currentPieceCount - 3].X--;
+					Tetromino.PieceList[currentPieceCount - 4].Y += 2;
+
+					CurrentRotation = 180;
+
+					goto BreakOut;
+				}
+			}
+		BreakOut:;
+		}
+		private static void ResetVals()
+		{
+			row19 = 0;
+		}
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+			spriteBatch.Begin();
+			foreach (Tetromino piece in Tetromino.PieceList)
+			{
+				spriteBatch.Draw(tile, new Vector2(piece.X * 25, piece.Y * 25), Color.White);
+			}
+			//spriteBatch.Draw(background, new Vector2(0,0), Color.White);
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
 }
